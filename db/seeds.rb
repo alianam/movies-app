c5163eb285b24805349dc4f90bc2995966f9ceab
alianam <melendeza5@gmail.com>
2017-10-25 21:10:57 -0400

    Remove extra from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,42 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'csv'
-# require 'awesome_print'
+require 'csv'
+require 'awesome_print'
 
 # seed the table with movies and their data from csv
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'movie_metadata.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'movie_metadata.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
-# csv.each do |row|
-#   new_movie = Movie.create!(
-#   	title: row['movie_title'],
-# 	  gross: row['gross'],
-# 	  director: row['director_name'],
-# 	  duration: row['duration'],
-# 	  imdb_link: row['movie_imdb_link']
-# 	)
-# 	genre_array = row['genres'].split('|')
-# 	genre_array.each do |genre|
-# 		new_genre = Genre.find_or_create_by(category: genre)
-# 		GenreMovie.create!(
-# 			movie: new_movie,
-# 			genre: new_genre
-# 		)
-# 	end
-# end
-
-new_movies = Feed.
-# start at index 2
-new_movie_titles = Feed.title('newmovies')
-
-
-
-Movie.new(
-  title: 
-)
-
-
-
-
+csv.each do |row|
+  new_movie = Movie.create!(
+  	title: row['movie_title'],
+	  gross: row['gross'],
+	  director: row['director_name'],
+	  duration: row['duration'],
+	  imdb_link: row['movie_imdb_link']
+	)
+	genre_array = row['genres'].split('|')
+	genre_array.each do |genre|
+		new_genre = Genre.find_or_create_by(category: genre)
+		GenreMovie.create!(
+			movie: new_movie,
+			genre: new_genre
+		)
+	end
+end
